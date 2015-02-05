7c326abe5b053c4b44a78e52eae2ab7f98ad10bf
Vito Librarius <vitolibrarius@gmail.com>
2015-02-04 20:15:38 -0700

    Added footer version number and site links

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -2,8 +2,19 @@
 		</div> <!-- content -->
 
 		<div id="ajaxSpinner" class="spinner"></div>
-		
-		<footer></footer>
+
+		<footer>
+			<div id="github">
+				<a href="https://github.com/vitolibrarius/contenta"> <?php echo $this->globalLabel("WebsiteLink", "Website"); ?></a> |
+				<a href="https://github.com/vitolibrarius/contenta/wiki"> <?php echo $this->globalLabel("HelpLink", "Help"); ?></a> |
+				<a href="https://github.com/vitolibrarius/contenta/issues/new"> <?php echo $this->globalLabel("AddIssueLink", "Report Problem"); ?></a>
+			</div>
+			<div id="version">
+				<?php echo $this->globalLabel("Version"); ?>: <?php echo currentVersionNumber(); ?>
+                <br>
+				<em><?php echo currentVersionHash(); ?></em>
+			</div>
+		</footer>
 	</div> <!-- container -->
 </body>
 </html>
