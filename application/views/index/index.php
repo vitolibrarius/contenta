e29c38f5759a7256e4edff78bf1975934784e899
Vito Librarius <vitolibrarius@gmail.com>
2015-02-05 20:19:05 -0700

    Some small adjustments to localization

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,23 +1,3 @@
-<?php if (Session::get('user_logged_in') == false):?>
-<form name='login' style="display:block; width:200px; margin-left:auto; margin-right:auto;" action="<?php echo Config::Web('/login/login') ?>" method="post">
-	<fieldset>
-	<legend>Login</legend>
-
-	<label for='username'>Username (or email)</label>
-	<input type="text" id='username' placeholder="JohnDoe" name="user_name" required />
-
-	<label for='password'>Password</label>
-	<input type="password" id='password' placeholder="" name="user_password" required />
-
-	<label class="remember-me-label">Keep me logged in (for 2 weeks)</label>
-	<input type="checkbox" name="user_rememberme" class="remember-me-checkbox" />
-
-	<label>&nbsp; </label>
-	<input type="submit" class="login-submit-button" />
-	</fieldset>
-</form>
-<?php endif; ?>
-
 <?php if (isset($this->marquee)): ?>
 <div id="marquee">
 	<div class="photobanner">
