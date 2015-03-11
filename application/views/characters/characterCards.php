52aecccf28f727559ddd078360730d49d41f5038
Vito Librarius <vitolibrarius@gmail.com>
2015-03-10 18:13:57 -0600

    Missed a file

---
+++ application/views/characters/characterCards.php
@@ -0,0 +1,24 @@
+<?php if (empty($this->listArray)): ?>
+	<div style="background:hsl(326,50%,75%)">
+		There are no matching records
+	</div>
+<?php else: ?>
+	<?php
+		$card = new html\Card();
+		$card->setDetailKeys( array(
+			model\Character::publisher_id => "publisherName",
+			model\Character::gender => model\Character::gender,
+			model\Character::realname => model\Character::realname
+			)
+		);
+		foreach($this->listArray as $key => $value) {
+			if ( isset($this->editAction) ) {
+				$card->setEditPath( $this->editAction . '/' . $value->id );
+			}
+			if ( isset($this->deleteAction) ) {
+	 			$card->setDeletePath( $this->deleteAction . '/' . $value->id );
+			}
+			echo $card->render($value);
+		}
+	?>
+<?php endif; ?>
