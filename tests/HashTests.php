9e40c72ff806b4ed3211b4a1bf3104794f5025bf
Vito Librarius <vitolibrarius@gmail.com>
2015-04-10 10:59:03 -0600

    Small test to examine the various hash algorithms

---
+++ tests/HashTests.php
@@ -0,0 +1,42 @@
+<?php
+
+	$system_path = dirname(dirname(__FILE__));
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
+	}
+
+	define('SYSTEM_PATH', str_replace("\\", "/", $system_path));
+	define('APPLICATION_PATH', SYSTEM_PATH . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR);
+
+	require SYSTEM_PATH .'application/config/bootstrap.php';
+	require SYSTEM_PATH .'application/config/autoload.php';
+	require SYSTEM_PATH .'application/config/common.php';
+	require SYSTEM_PATH .'application/config/errors.php';
+	require SYSTEM_PATH .'application/libs/Config.php';
+	require SYSTEM_PATH .'application/libs/Cache.php';
+
+	require SYSTEM_PATH .'tests/_ResetConfig.php';
+	require SYSTEM_PATH .'tests/_Data.php';
+
+$options = getopt( "f:");
+echo "Options " . var_export($options, true) . PHP_EOL. PHP_EOL;
+
+if (is_file($options['f']) ) {
+
+	my_echo( );
+	my_echo( "---------- Hashes ");
+	print_r(hash_algos());
+
+	my_echo( );
+	my_echo( "---------- Hashes ");
+	foreach ( hash_algos() as $algo ) {
+		$start = microtime(true);;
+		$hash = hash_file( $algo , $options['f'] );
+		my_echo( str_pad ( $algo, 12) . " " . (microtime(true) - $start) . "\t $hash \t\t " );
+	}
+}
+else {
+	my_echo( );
+	my_echo( "---------- file not found for " . $options['f']);
+}
