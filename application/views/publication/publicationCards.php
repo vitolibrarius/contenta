cb0e971684e37fd935c0aac3fec42e57fbbe2566
Vito Librarius <vitolibrarius@gmail.com>
2015-08-02 14:37:21 -0600

    Details for the publication card

--- application/views/publication/publicationCards.php
+++ application/views/publication/publicationCards.php
@@ -5,7 +5,11 @@
 <?php else: ?>
 	<?php
 		$card = new html\Card();
+		$card->setDisplayDescriptionKey( "shortDescription" );
 		$card->setDetailKeys( array(
+			model\Publication::series_id => "series/name",
+			model\Publication::issue_num => "issue_num",
+			model\Publication::pub_date => "publishedMonthYear",
 			)
 		);
 		if ( is_null($this->listArray) || count($this->listArray) == 0) {
