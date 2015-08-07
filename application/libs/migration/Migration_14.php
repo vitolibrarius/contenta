739e09df1f384e11c7019002f6fa8efcf3b92253
Vito Librarius <vitolibrarius@gmail.com>
2015-08-07 10:07:24 -0600

    Starting to collect SQL updates for next release

---
+++ application/libs/migration/Migration_14.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace migration;
+
+use \Migrator as Migrator;
+use \MigrationFailedException as MigrationFailedException;
+use \Config as Config;
+use \Logger as Logger;
+use \Model as Model;
+use \SQL as SQL;
+
+use utilities\CronEvaluator as CronEvaluator;
+use db\Qualifier as Qualifier;
+
+use model\Series as Series;
+use model\Publisher as Publisher;
+use model\Series_Alias as Series_Alias;
+use model\Character as Character;
+use model\Character_Alias as Character_Alias;
+use model\Series_Character as Series_Character;
+use model\Users as Users;
+use model\User_Series as User_Series;
+use model\Story_Arc as Story_Arc;
+use model\Story_Arc_Character as Story_Arc_Character;
+use model\Story_Arc_Series as Story_Arc_Series;
+use model\Story_Arc_Publication as Story_Arc_Publication;
+use model\Publication as Publication;
+use model\Publication_Character as Publication_Character;
+use model\Media_Type as Media_Type;
+use model\Media as Media;
+use model\Endpoint_Type as Endpoint_Type;
+use model\Endpoint as Endpoint;
+use model\RSS as RSS;
+use model\Job_Type as Job_Type;
+
+
+class Migration_14 extends Migrator
+{
+	public function sqlite_preUpgrade()
+	{
+		// backup sqlite database file
+		$db_path = Config::GetPath("Database/path", null);
+		if ( strlen($db_path) == 0 ) {
+			throw new \Exception('No path set in configuration for sqlite database');
+		}
+		$db_file = appendPath($db_path, "contenta.sqlite" );
+		$backupDatabase = appendPath($this->scratch, "contenta.Migration_14." . date('Y-m-d.H-i-s') . ".backup");
+		file_exists($db_file) == false || copy($db_file, $backupDatabase) || die('Failed to backup ' . $db_file);
+	}
+
+	public function sqlite_upgrade()
+	{
+	}
+
+	public function sqlite_postUpgrade()
+	{
+		$job_type_model = Model::Named("Job_Type");
+		$types = array(
+			array(
+				Job_Type::name => "Publication Updates",
+				Job_Type::code => "publication",
+				Job_Type::desc => "Load publication data from metadata endpoints (like ComicVine) in batches of 20",
+				Job_Type::processor => "ComicVineImporter",
+				Job_Type::parameter => json_encode(array( "enqueueBatch" => array("publication", 20)), JSON_PRETTY_PRINT),
+				Job_Type::scheduled => Model::TERTIARY_TRUE,
+				Job_Type::requires_endpoint => Model::TERTIARY_TRUE
+			)
+		);
+
+		foreach ($types as $dict) {
+			$existing = \SQL::raw( "select id FROM " . Job_Type::TABLE . " where code = :code", array( ":code" => $dict[Job_Type::code]));
+			if ( is_array($existing) && count($existing) > 0) {
+				$update = \SQL::Update($job_type_model, Qualifier::Equals( "code", $dict[Job_Type::code]), $dict);
+				$update->commitTransaction();
+			}
+			else {
+				$inserts = \SQL::Insert( $job_type_model, array(
+						"name",
+						"code",
+						"desc",
+						"processor",
+						"scheduled",
+						"parameter",
+						"requires_endpoint"
+					)
+				);
+				$inserts->addRecord( $dict );
+				$inserts->commitTransaction(true);
+			}
+		}
+
+		return true;
+	}
+}
