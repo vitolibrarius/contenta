da69e3e0af4a696ab45d036a828ff2353472001d
Vito Librarius <vitolibrarius@gmail.com>
2015-09-03 21:17:04 -0600

    added basic git commit hooks

---
+++ application/views/dav/serverVars.php
@@ -0,0 +1,55 @@
+<?php
+
+$keys = array(
+	'PHP_SELF',
+	'argv',
+	'argc',
+	'GATEWAY_INTERFACE',
+	'SERVER_ADDR',
+	'SERVER_NAME',
+	'SERVER_SOFTWARE',
+	'SERVER_PROTOCOL',
+	'REQUEST_METHOD',
+	'REQUEST_TIME',
+	'REQUEST_TIME_FLOAT',
+	'QUERY_STRING',
+	'DOCUMENT_ROOT',
+	'HTTP_ACCEPT',
+	'HTTP_ACCEPT_CHARSET',
+	'HTTP_ACCEPT_ENCODING',
+	'HTTP_ACCEPT_LANGUAGE',
+	'HTTP_CONNECTION',
+	'HTTP_HOST',
+	'HTTP_REFERER',
+	'HTTP_USER_AGENT',
+	'HTTPS',
+	'REMOTE_ADDR',
+	'REMOTE_HOST',
+	'REMOTE_PORT',
+	'REMOTE_USER',
+	'REDIRECT_REMOTE_USER',
+	'SCRIPT_FILENAME',
+	'SERVER_ADMIN',
+	'SERVER_PORT',
+	'SERVER_SIGNATURE',
+	'PATH_TRANSLATED',
+	'SCRIPT_NAME',
+	'REQUEST_URI',
+	'PHP_AUTH_DIGEST',
+	'PHP_AUTH_USER',
+	'PHP_AUTH_PW',
+	'AUTH_TYPE',
+	'PATH_INFO',
+	'ORIG_PATH_INFO'
+);
+?>
+
+<table cellpadding="10">
+<?php foreach ($keys as $akey) : ?>
+    <?php if (isset($_SERVER[$akey])) : ?>
+        <tr><td><?php echo $akey; ?></td><td><?php echo $_SERVER[$akey]; ?></td></tr>
+    <?php else : ?>
+        <tr><td><?php echo $akey; ?></td><td> - </td></tr>
+    <?php endif; ?>
+<?php endforeach; ?>
+</table>
