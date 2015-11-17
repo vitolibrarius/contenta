0780959035b4f1586c33f28849da1e88657fc233
Vito Librarius <vitolibrarius@gmail.com>
2015-11-17 10:25:24 -0700

    Issue #82	Misc typos - added new script to merge branch changes into master but not delete the branch

---
+++ scripts/git/MergeUpBranch.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+STATUS=`git status -s | wc -l`
+if [ $STATUS -ne 0 ]; then
+	echo "You have uncommited changes"
+	echo
+	git status -s
+	exit 1
+fi
+
+BRANCH=`git branch | grep "*" | awk '{print $2}'`
+
+if [ $BRANCH != "master" ]; then
+	git checkout master
+	git merge $BRANCH
+	chmod a+w localization/*.json
+fi
+
