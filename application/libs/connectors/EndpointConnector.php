7c3e410e38211d643bd6861d9dc3a04bb1189247
Vito Librarius <vitolibrarius@gmail.com>
2017-09-13 22:19:18 -0600

    ffs

--- application/libs/connectors/EndpointConnector.php
+++ application/libs/connectors/EndpointConnector.php
@@ -198,15 +198,15 @@ abstract class EndpointConnector
 	}
 
 	public function overDailyMaximum() {
-		if ( is_null( $this->maxRequests ) && isset($this->endpoint) ) {
-			$ep = $this->endpoint();
-			$name = $ep->pkValue() . '_' . $ep->displayName();
-			$this->maxRequests = new EndpointRequestCounter( $name, $ep->daily_max );
-		}
-
-		if ( is_null( $this->maxRequests ) == false ) {
-			return $this->maxRequests->overMaximum();
-		}
+// 		if ( is_null( $this->maxRequests ) && isset($this->endpoint) ) {
+// 			$ep = $this->endpoint();
+// 			$name = $ep->pkValue() . '_' . $ep->displayName();
+// 			$this->maxRequests = new EndpointRequestCounter( $name, $ep->daily_max );
+// 		}
+//
+// 		if ( is_null( $this->maxRequests ) == false ) {
+// 			return $this->maxRequests->overMaximum();
+// 		}
 		return false;
 	}
 
